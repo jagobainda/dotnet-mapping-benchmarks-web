6edc7b654881781ab68978df292ec4b0159e1b2a
jagobainda <jagobainda@gmail.com>
2026-03-30 03:07:31 +0200

    fix: update sitemap URL in robots.txt and enhance sitemap.xml with last modified date and change frequency

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -4,11 +4,14 @@ const site = 'https://benchmarks.jagoba.dev';
 const base = '/dotnet-mappers';
 
 export const GET: APIRoute = () => {
+  const lastmod = new Date().toISOString().split('T')[0];
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
         <loc>${site}${base}/</loc>
-        <changefreq>always</changefreq>
+        <lastmod>${lastmod}</lastmod> 
+        <changefreq>hourly</changefreq>
         <priority>1.0</priority>
       </url>
     </urlset>`;
