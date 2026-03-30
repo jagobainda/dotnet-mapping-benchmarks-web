9eef20b41c625023c09c3d72b7720f7c3a0a9352
jagobainda <jagobainda@gmail.com>
2026-03-30 02:27:23 +0200

    fix: update sitemap URL to include base path for .NET Mappers

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,12 +1,13 @@
 import type { APIRoute } from 'astro';
 
 const site = 'https://benchmarks.jagoba.dev';
+const base = '/dotnet-mappers';
 
 export const GET: APIRoute = () => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
-        <loc>${site}/</loc>
+        <loc>${site}${base}/</loc>
         <changefreq>always</changefreq>
         <priority>1.0</priority>
       </url>
